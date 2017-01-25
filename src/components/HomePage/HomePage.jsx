83470836f56c2e37d740772af8e9ea5bd92219f1
jennyjd <night994@gmail.com>
2017-01-25 20:30:23 +0300

    Replace some stylesheets with SASS

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header.jsx';
 import Footer from '../Footer/Footer.jsx';
-import './HomePage.css';
+import './HomePage.sass';
 
 class HomePage extends React.Component{
 
 	render(){
 		return(
-			<div className="main">
-				<div className="form">
+			<div id="main">
+				<div id="panel">
 					<span id="welcome">Welcome to MyBlog</span>
 					<button id="try-now">Try Now</button>
 				</div>
